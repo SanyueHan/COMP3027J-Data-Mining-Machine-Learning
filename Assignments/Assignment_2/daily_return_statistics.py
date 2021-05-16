6ca712b5dc4381542005e07f1befc5cba8193363
Laniskefu <hansanyue@hotmail.com>
2021-05-16 18:16:58 +0800

    use api to download historical data

---
+++ Assignments/Assignment_2/daily_return_statistics.py
@@ -0,0 +1,33 @@
+import os
+
+DIR = "data/pretest/pretest_answer/"
+
+
+def calculate_return(close_final, close_init):
+    return 100 * (close_final - close_init) / close_init
+
+
+def calculate_returns(path):
+    returns = []
+    with open(path, "r") as history:
+        closes = []
+        for line in history:
+            try:
+                close = float(line.split(',')[4])
+            except ValueError:
+                continue
+            closes.append(close)
+            if len(closes) == 3:
+                closes.pop(0)
+            if len(closes) == 2:
+                returns.append(calculate_return(closes[1], closes[0]))
+    average = sum(returns)/len(returns)
+    return [average, max(returns), min(returns), sum([(r-average)**2 for r in returns])/len(returns)]
+
+
+if __name__ == '__main__':
+    with open("daily_return_statistics.csv", "w") as answer:
+        answer.write("tickers,average_return,max_return,min_return,return_variance\n")
+        for name in sorted(os.listdir(DIR)):
+            contents = [name.split('.')[0], *[f"{r:.2f}" for r in calculate_returns(DIR+name)]]
+            answer.write(','.join(contents) + '\n')
---
+++ Assignments/Assignment_2/data/yahoo_finance.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+import pandas as pd
+
+
+stock_ticker_list = pd.read_csv('Stock_Ticker_List.csv')
+
+
+for stock in stock_ticker_list['Symbol']:
+    yf.download(stock).to_csv('api/' + stock + '.csv')
+    print(stock, 'finished')
+
